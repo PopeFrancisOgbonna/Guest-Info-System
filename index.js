b6ad2b373f5f6ec4526926ef618630da1d644d38
PopeFrancisOgbonna <francisogbonna24@gmail.com>
2020-07-22 13:34:38 +0100

    changed connection string

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const viewGuest = require('./models/handleGuest');
 
 const db = knex({
     client: 'pg',
-    connectionString: process.env.DATABASE_URL,
+    connectionString:{
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    },
     ssl:true
 });
 

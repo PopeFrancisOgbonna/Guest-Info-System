45b961832bfcbaa283e7e3780cc6c434c8795c95
PopeFrancisOgbonna <francisogbonna24@gmail.com>
2020-07-23 07:02:16 +0100

    made changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const db = new pg.Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
       }

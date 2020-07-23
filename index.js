def4327f58c1768833e1e339bfb3c78598bd0225
PopeFrancisOgbonna <francisogbonna24@gmail.com>
2020-07-23 06:49:31 +0100

    changed ssl

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const db = new pg.Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: isProduction,
+    ssl: true
   });
   db.on('connect', () => {
     console.log('Teamwork Database connected successfully!');
